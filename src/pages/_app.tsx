e8da8703322cb40f9636d0f45821435302a99f0f
eduardobattisti <eduardobattisti@protonmail.com>
2021-04-22 22:45:48 -0300

    created the play and pause function. Update the PlayerContext, _app and index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,49 @@ import { Player } from "../components/Player";
 import { PlayerContext } from "../contexts/PlayerContexts";
 
 import styles from "../styles/app.module.scss";
+
 import React from "react";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <PlayerContext.Provider value={"Diego"}>
-            <div className={styles.wrapper}>
-                <main>
-                    <Header />
-                    <Component {...pageProps} />
-                </main>
-                <Player />
-            </div>
-        </PlayerContext.Provider>
-    );
+	const [episodeList, setEpisodeList] = useState([]);
+	const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	function play(episode) {
+		setEpisodeList([episode]);
+		setCurrentEpisodeIndex(0);
+		setIsPlaying(true);
+	}
+
+	function togglePlay() {
+		setIsPlaying(!isPlaying);
+	}
+
+	function setPlayingState(state: boolean) {
+		setIsPlaying(state);
+	}
+
+	return (
+		<PlayerContext.Provider
+			value={{
+				episodeList,
+				currentEpisodeIndex,
+				play,
+				isPlaying,
+				togglePlay,
+				setPlayingState,
+			}}
+		>
+			<div className={styles.wrapper}>
+				<main>
+					<Header />
+					<Component {...pageProps} />
+				</main>
+				<Player />
+			</div>
+		</PlayerContext.Provider>
+	);
 }
 
 export default MyApp;
